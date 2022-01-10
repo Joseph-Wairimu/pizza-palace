0aca6995e2b1b43556599d30cdf376f358daa8c3
Joseph-Wairimu <josephkamore084@gmail.com>
2022-01-10 14:35:08 +0300

    adding an array for the pizzas

--- js/script.js
+++ js/script.js
@@ -1,1 +1,39 @@
 let button= document.getElementById("#button");
+
+let pizza =[
+{ 
+    Name:'BBQ Chicken Pizza',
+    Size=['small','medium','large'],
+    crust=['crispy','stuffed','gluten-free'],
+    topping=['pepperoni','black olives','fresh garlic']
+
+},
+{ 
+    Name:'Hawaiian Pizza',
+    Size=['small','medium','large'],
+    crust=['crispy','stuffed','gluten-free'],
+    topping=['pepperoni','black olives','fresh garlic']
+
+},
+{ 
+    Name:'Margherita Pizza',
+    Size=['small','medium','large'],
+    crust=['crispy','stuffed','gluten-free'],
+    topping=['pepperoni','black olives','fresh garlic']
+
+},
+{ 
+    Name:'Supreme Pizza',
+    Size=['small','medium','large'],
+    crust=['crispy','stuffed','gluten-free'],
+    topping=['pepperoni','black olives','fresh garlic']
+
+},
+{ 
+    Name:'Buffalo Pizza',
+    Size=['small','medium','large'],
+    crust=['crispy','stuffed','gluten-free'],
+    topping=['pepperoni','black olives','fresh garlic']
+
+}
+];

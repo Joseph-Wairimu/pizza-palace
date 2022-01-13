1d6c224558d624b649747b069c12e84d80943fee
Joseph-Wairimu <josephkamore084@gmail.com>
2022-01-13 17:26:34 +0300

    function to show selected option of pizza in innerHTML

--- js/script.js
+++ js/script.js
@@ -6,3 +6,7 @@ function PizzaOrder(size, crust, toppings) {
     this.quantity = number;
     this.price = 0;
 };
+pizzaSelectChange = function () {
+    this.flavor = $("#flavor option:selected").val();
+    document.getElementById("pflavor").innerHTML = this.flavor;
+}

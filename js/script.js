540abe2e74f2659acebbd806e69e3a2efc9a2f37
Joseph-Wairimu <josephkamore084@gmail.com>
2022-01-13 17:30:13 +0300

    function to show selected pizza size ,topping and crust in innerHTML

--- js/script.js
+++ js/script.js
@@ -10,3 +10,18 @@ pizzaSelectChange = function () {
     this.flavor = $("#flavor option:selected").val();
     document.getElementById("pflavor").innerHTML = this.flavor;
 }
+proportionSelectChange = function () {
+    this.size = $("#size option:selected").val();
+    document.getElementById("psize").innerHTML = this.size;
+    
+    
+}
+crustySelectChange = function () {
+    this.crust = $("#crust option:selected").val();
+    document.getElementById("pcrust").innerHTML = this.crust;
+}
+toppySelectChange = function () {
+    this.topping = $("#topping option:selected").val();
+    document.getElementById("ptoppings").innerHTML = this.topping;
+   
+}
